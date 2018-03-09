73cf98dcf28905fac2df925b24426a5ae46a6b43
wwey <63837847@qq.com>
2018-03-09 16:01:42 +0800

    加了一个getter setter 方法

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,7 +1,7 @@
 public class User {
 
      private Integer id;
-
+private String name;
      public User(){
 
      }
@@ -12,5 +12,13 @@ public class User {
 
     public void setId(Integer id) {
         this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
